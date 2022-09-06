84a4f2f973f356743715564252fa7439e2946580
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-09-06 17:12:53 -0500

    cambio en vista

--- ActividadAlgoritmia1/1.cpp
+++ ActividadAlgoritmia1/1.cpp
@@ -10,7 +10,7 @@ int main()
         a= new int [N];
         for(j=0;j<N;j++){
             a[j]=j*i;
-            //cout<< "numero" <<a[j]<<endl;
+            cout<< "numero" <<a[j]<<endl;
             cout<< "direccion: " << &a[j]<<endl;
         }
         delete []a;
