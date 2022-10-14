66e71988b9ee80e27d685deb52d7cd21d967a922
camilo-a-g-z <65404417+camilo-a-g-z@users.noreply.github.com>
2022-10-14 13:16:27 -0500

    Solucion errores

--- Lista/Lista.h
+++ Lista/Lista.h
@@ -1,6 +1,6 @@
 #include <iostream>
-#ifndef PARQUEADERO_H
-#define PARQUEADERO_H
+#ifndef LISTA_H
+#define LISTA_H
 
 template < class T >  
 //Estructura a emplear para lista
@@ -21,11 +21,62 @@ class Lista
 		{
 			cab = NULL ; tam = 0;
 		};
-		~Lista();
+		~Lista<T>();*/
 		bool ListaVacia();
 		int TamLista();
 		bool Insertar(T dato, int pos);
 		T Eliminar(int pos);
 		T ObtenerDatos(int pos);
 };
+template < class T >  
+
+bool Lista<T>::ListaVacia(){return (tam == 0);}
+template < class T >  
+int Lista<T>::TamLista(){return tam;}
+
+template < class T >  
+
+bool Lista<T>::Insertar(T dato, int pos){
+	//en el caso de que estemos pidiendo un lugar que no se puede
+	if(pos > tam + 1){return false;}
+	//se crea nuevo nodo y variables auxiliares necesarias
+	Nodo<T> *n_n = new Nodo<T>;
+	Nodo<T> *aux, *aux2;
+	
+	n_n->info = dato;
+	if(pos == 1)
+	{
+		n_n->sig=cab;
+		cab=n_n;
+	} else if(pos == tam+1)
+	{
+		aux = cab;
+		while(aux->sig != NULL) {aux = aux->sig;}
+		aux->sig = n_n;
+		n_n->sig = NULL;
+	}else
+	{
+		aux = cab;
+		for(int i = 1; i < pos-1; i++){aux = aux->sig;}
+		aux2 = aux->sig;
+		n_n->sig = aux2;
+		aux->sig = n_n;
+	}
+	tam++;
+	return true;
+}
+
+template < class T >  
+T Lista<T>::Eliminar(int pos){
+	if(pos > tam + 1){return NULL;}
+	Nodo<T> *aux, *aux2;
+	Nodo<T> n_d = new Nodo<T>;
+	for(int i = 1; i < pos-1; i++){aux = aux->sig;}
+	aux2 = aux->sig;
+	aux->sig = aux2->sig;
+	n_d->info = aux2->info;
+	n_d->sig = aux2->sig;
+	delete aux2;
+	return n_d;
+}
 #endif
