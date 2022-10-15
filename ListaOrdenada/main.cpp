a7865da8bee658b4c214537911491463b16dcd0a
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-10-15 01:10:20 -0500

    Se organiza main y se ponen requerimientos dados en clase

--- ListaOrdenada/main.cpp
+++ ListaOrdenada/main.cpp
@@ -3,20 +3,37 @@
 
 using namespace std;
 
+void presentacion();
+
 int main(int argc, char** argv) {
-	//template < class T >  
-	
+	presentacion();
+	return 0;
+}
+
+void presentacion(){
+	/*
+		Requerimientos: 
+		Crear una lista de enteros, reales y carácteres.	*Hecho
+		Llenar cada lista con 6 elementos.					*Hecho
+		Imprimir la lista ordenada.							*Hecho
+		Eliminar 2 o 3 elementos.
+		Imprimir la nueva lista ordenada con dichos elementos eliminados	*Hecho
+	*/
 	Lista<int> enteros ;
 	Lista<float> reales;
 	Lista<char> caracteres;
 	enteros.Insertar(6);
-	enteros.Insertar(9);
+	enteros.Insertar(90);
 	enteros.Insertar(3);
 	enteros.Insertar(12);
 	enteros.Insertar(10);	
+	enteros.Insertar(11);	
 	cout<<"Lista completa: ";
 	enteros.mostrar();
 	cout<<"Se elimina un dato: "<<enteros.Eliminar(2)<<endl;
+	cout<<"Lista actual: ";
+	enteros.mostrar();
+	cout<<"Se elimina un dato: "<<enteros.Eliminar(1)<<endl;
 	cout<<"Tamaño: "<<enteros.TamLista()<<endl;
 	cout<<"Lista actual: ";
 	enteros.mostrar();
@@ -28,9 +45,13 @@ int main(int argc, char** argv) {
 	reales.Insertar(3.8);
 	reales.Insertar(12.0);
 	reales.Insertar(10.9);
+	reales.Insertar(1.1);
 	cout<<"Lista completa: ";
 	reales.mostrar();
 	cout<<"Se elimina un dato: "<<reales.Eliminar(3)<<endl;
+	cout<<"Lista actual: ";
+	reales.mostrar();
+	cout<<"Se elimina un dato: "<<reales.Eliminar(4)<<endl;
 	cout<<"Tamaño: "<<reales.TamLista()<<endl;
 	cout<<"Lista actual: ";
 	reales.mostrar();
@@ -42,12 +63,14 @@ int main(int argc, char** argv) {
 	caracteres.Insertar('f');
 	caracteres.Insertar('m');
 	caracteres.Insertar('p');
+	caracteres.Insertar('b');
 	cout<<"Lista completa: ";
 	caracteres.mostrar();
 	cout<<"Se elimina un dato: "<<caracteres.Eliminar(4)<<endl;
+	cout<<"Lista actual: ";
+	caracteres.mostrar();
+	cout<<"Se elimina un dato: "<<caracteres.Eliminar(1)<<endl;
 	cout<<"Tamaño: "<<caracteres.TamLista()<<endl;
 	cout<<"Lista actual: ";
 	caracteres.mostrar();
-	return 0;
 }
-
