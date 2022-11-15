0d3332d99f690abf6adf96528512c3b2d0a735b4
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-11-15 16:19:05 -0500

    Creacion de funciones de clase

--- Colas de Prioridad/Monticulo.h
+++ Colas de Prioridad/Monticulo.h
@@ -3,18 +3,29 @@
 #define MONTICULO_H
 using namespace std;
 
-class monticulo{ 
+class Monticulo{ 
 	private:
 		int *Arr, tam, PosAct;
 	public: 
-		monticulo(int);
+		Monticulo(int tam){
+			this.tam = tam;
+			Arr = new int[tam];
+			//posicion 0 de control para saber posicion libre, para saber por donde crecer
+			Arr[0] = 1;
+		}
 		bool insertar(int);
 		int atender();
 		void imprimirArreglo();
 		bool monticulo_lleno();
-
 };
+bool Monticulo::monticulo_lleno(){
+	return (Arr[0] == tam-1) ? true : false;
+}
 
+void Monticulo::imprimirArreglo(){
+	for(int i=1;i<tam;i++){cout<<Arr[i]<<" | ";}
+	cout<<endl;
+}
 
 
 #endif
--- Colas de Prioridad/main.cpp
+++ Colas de Prioridad/main.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-	
+	Monticulo m(5);
 	return 0;
 }
