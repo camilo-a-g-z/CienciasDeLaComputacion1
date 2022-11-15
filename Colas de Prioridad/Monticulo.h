d7f219842fda68657224ff9e6282d5db58ab2c4a
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-11-15 18:13:19 -0500

    Se crea funcion para insertar datos

--- Colas de Prioridad/Monticulo.h
+++ Colas de Prioridad/Monticulo.h
@@ -7,25 +7,47 @@ class Monticulo{
 	private:
 		int *Arr, tam, PosAct;
 	public: 
-		Monticulo(int tam){
-			this.tam = tam;
+		Monticulo(int t){
+			tam = t;
 			Arr = new int[tam];
 			//posicion 0 de control para saber posicion libre, para saber por donde crecer
 			Arr[0] = 1;
+			for(int i=1;i<tam;i++){Arr[i]=0;}
 		}
 		bool insertar(int);
 		int atender();
 		void imprimirArreglo();
 		bool monticulo_lleno();
+		void organizar(int);
 };
-bool Monticulo::monticulo_lleno(){
-	return (Arr[0] == tam-1) ? true : false;
-}
+bool Monticulo::monticulo_lleno(){return (Arr[0] == tam) ? true : false;}
 
 void Monticulo::imprimirArreglo(){
 	for(int i=1;i<tam;i++){cout<<Arr[i]<<" | ";}
 	cout<<endl;
 }
 
+bool Monticulo::insertar(int d){
+	if(!monticulo_lleno()){
+		Arr[Arr[0]] = d;
+		cout<<"val Arr0: "<<Arr[0]<<"Con dato: "<<d<<endl;
+		organizar(Arr[0]);
+		Arr[0] = Arr[0]+1;
+		return true;
+	}else{return false;}
+}
 
+void Monticulo::organizar(int pos){
+	int pad;
+	if(pos != 1){
+		pad = pos/2;
+		if(Arr[pad]<Arr[pos]){
+			int aux=Arr[pad];
+			Arr[pad] = Arr[pos]; 
+			Arr[pos] = aux;
+			organizar(pad);
+		}		
+	}
+}
+	
 #endif
--- Colas de Prioridad/main.cpp
+++ Colas de Prioridad/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main(){
-	Monticulo m(5);
+	Monticulo m(11);
+	m.insertar(60);
+	m.insertar(80);
+	m.insertar(20);
+	m.insertar(90);
+	m.insertar(50);
+	m.insertar(40);
+	m.insertar(30);
+	m.insertar(70);
+	m.insertar(55);
+	m.insertar(45);
+	m.imprimirArreglo();
 	return 0;
 }
